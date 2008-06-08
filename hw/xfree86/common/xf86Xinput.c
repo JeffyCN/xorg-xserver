068e68e206223aef2b6b85fc9b2468589645298a
Peter Hutterer <peter@cs.unisa.edu.au>
2008-06-08 23:38:45 +0930

    xfree86: Fix up xf86ScaleAxis
    Some driver still call it, so we might as well work correctly. Always
    resetting X to the Sxhigh is sub-optimal.
    (cherry picked from commit a3a7c12fcf8e4ac1418f9ea53f76091f309a721b)

--- hw/xfree86/common/xf86Xinput.c
+++ hw/xfree86/common/xf86Xinput.c
@@ -737,9 +737,9 @@ xf86ScaleAxis(int	Cx,
 	ErrorF ("Divide by Zero in xf86ScaleAxis");
     }
     
-    if (X > Sxlow)
+    if (X > Sxhigh)
 	X = Sxlow;
-    if (X < Sxhigh)
+    if (X < Sxlow)
 	X = Sxhigh;
     
     return (X);
