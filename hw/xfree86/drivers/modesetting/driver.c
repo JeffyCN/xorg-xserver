f70443f8dc661e1c27a6e47faa84993e22938eed
Jeffy Chen <jeffy.chen@rock-chips.com>
2019-07-29 14:26:11 +0800

    modesetting: Support exa with rockchip rga 2d accel
    Implementing neccessarily exa APIs for basic window moving and
    screen transforms.
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- hw/xfree86/drivers/modesetting/driver.c
+++ hw/xfree86/drivers/modesetting/driver.c
@@ -772,6 +772,20 @@ FreeRec(ScrnInfoPtr pScrn)
 }
 
 static void
+try_enable_exa(ScrnInfoPtr pScrn)
+{
+    modesettingPtr ms = modesettingPTR(pScrn);
+
+    if (xf86LoadSubModule(pScrn, "exa"))
+        ms_init_exa(pScrn);
+
+    if (ms->drmmode.exa)
+        xf86DrvMsg(pScrn->scrnIndex, X_INFO, "exa initialized\n");
+    else
+        xf86DrvMsg(pScrn->scrnIndex, X_ERROR, "exa initialization failed\n");
+}
+
+static void
 try_enable_glamor(ScrnInfoPtr pScrn)
 {
     modesettingPtr ms = modesettingPTR(pScrn);
@@ -1233,10 +1247,16 @@ msUpdatePacked(ScreenPtr pScreen, shadowBufPtr pBuf)
         free(prect);
     } while (0);
 
+    if (ms->drmmode.exa)
+        ms_exa_prepare_access(pBuf->pPixmap, 0);
+
     if (use_3224)
         shadowUpdate32to24(pScreen, pBuf);
     else
         shadowUpdatePacked(pScreen, pBuf);
+
+    if (ms->drmmode.exa)
+        ms_exa_finish_access(pBuf->pPixmap, 0);
 }
 
 static Bool
@@ -1402,10 +1422,12 @@ CreateScreenResources(ScreenPtr pScreen)
     if (!ms->drmmode.sw_cursor)
         drmmode_map_cursor_bos(pScrn, &ms->drmmode);
 
-    if (!ms->drmmode.gbm) {
+    if (!ms->drmmode.gbm && !ms->drmmode.exa) {
         pixels = drmmode_map_front_bo(&ms->drmmode);
         if (!pixels)
             return FALSE;
+
+        drmmode_glamor_handle_new_screen_pixmap(&ms->drmmode);
     }
 
     rootPixmap = pScreen->GetScreenPixmap(pScreen);
@@ -1613,6 +1635,7 @@ ScreenInit(ScreenPtr pScreen, int argc, char **argv)
     ScrnInfoPtr pScrn = xf86ScreenToScrn(pScreen);
     modesettingPtr ms = modesettingPTR(pScrn);
     VisualPtr visual;
+    const char *str_value;
 
     pScrn->pScreen = pScreen;
 
@@ -1696,6 +1719,11 @@ ScreenInit(ScreenPtr pScreen, int argc, char **argv)
 
     xf86SetBlackWhitePixels(pScreen);
 
+    str_value = xf86GetOptValString(ms->drmmode.Options,
+                                    OPTION_ACCEL_METHOD);
+    if (str_value && !strcmp(str_value, "exa"))
+        try_enable_exa(pScrn);
+
     xf86SetBackingStore(pScreen);
     xf86SetSilkenMouse(pScreen);
     miDCInitialize(pScreen, xf86GetPointerScreenFuncs());
@@ -1758,6 +1786,16 @@ ScreenInit(ScreenPtr pScreen, int argc, char **argv)
                        "Failed to initialize XV support.\n");
     }
 #endif
+
+    if (ms->drmmode.exa) {
+        ms->adaptor = ms_exa_xv_init(pScreen, 16);
+        if (ms->adaptor != NULL) {
+            xf86XVScreenInit(pScreen, &ms->adaptor, 1);
+        } else {
+            xf86DrvMsg(pScrn->scrnIndex, X_ERROR,
+                       "Failed to initialize XV support.\n");
+        }
+    }
 
     if (serverGeneration == 1)
         xf86ShowUnusedOptions(pScrn->scrnIndex, pScrn->options);
@@ -1907,6 +1945,10 @@ CloseScreen(ScreenPtr pScreen)
 
     if (pScrn->vtSema) {
         LeaveVT(pScrn);
+    }
+
+    if (ms->drmmode.exa) {
+        ms_deinit_exa(pScrn);
     }
 
     pScreen->CreateScreenResources = ms->createScreenResources;
