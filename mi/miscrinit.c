d5405c04e6cde1754a5199d1c946ebfc2b7e074e
Jeffy Chen <jeffy.chen@rock-chips.com>
2019-08-29 11:24:35 +0800

    mi: miscrinit: Avoid breaking CloseScreen() chain
    Breaking CloseScreen() chain may cause resources leak.
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- mi/miscrinit.c
+++ mi/miscrinit.c
@@ -52,6 +52,17 @@ from The Open Group.
  * detail to the whole server.
  */
 
+/* per-screen private data */
+static DevPrivateKeyRec miScreenPrivKeyRec;
+
+#define miScreenPrivKey (&miScreenPrivKeyRec)
+
+typedef struct {
+    CloseScreenProcPtr CloseScreen;
+} miScreenRec, *miScreenPtr;
+
+#define miGetScreenPriv(s) ((miScreenPtr)(dixLookupPrivate(&(s)->devPrivates, miScreenPrivKey)))
+
 typedef struct {
     void *pbits;                /* pointer to framebuffer */
     int width;                  /* delta to add to a framebuffer addr to move one row down */
@@ -121,7 +132,18 @@ miModifyPixmapHeader(PixmapPtr pPixmap, int width, int height, int depth,
 static Bool
 miCloseScreen(ScreenPtr pScreen)
 {
-    return ((*pScreen->DestroyPixmap) ((PixmapPtr) pScreen->devPrivate));
+    miScreenPtr pScreenPriv = miGetScreenPriv(pScreen);
+
+    ((*pScreen->DestroyPixmap) ((PixmapPtr) pScreen->devPrivate));
+
+    pScreen->CloseScreen = pScreenPriv->CloseScreen;
+
+    free(pScreenPriv);
+
+    if (pScreen->CloseScreen)
+        return (*pScreen->CloseScreen) (pScreen);
+
+    return TRUE;
 }
 
 void
@@ -221,6 +243,17 @@ miScreenInit(ScreenPtr pScreen, void *pbits,  /* pointer to screen bits */
              VisualRec * visuals        /* supported visuals */
     )
 {
+    miScreenPtr pScreenPriv;
+
+    if (!dixRegisterPrivateKey(&miScreenPrivKeyRec, PRIVATE_SCREEN, 0))
+        return FALSE;
+
+    pScreenPriv = calloc(1, sizeof(miScreenRec));
+    if (!pScreenPriv)
+        return FALSE;
+
+    dixSetPrivate(&pScreen->devPrivates, miScreenPrivKey, pScreenPriv);
+
     pScreen->width = xsize;
     pScreen->height = ysize;
     pScreen->mmWidth = (xsize * 254 + dpix * 5) / (dpix * 10);
@@ -245,6 +278,7 @@ miScreenInit(ScreenPtr pScreen, void *pbits,  /* pointer to screen bits */
 #ifdef MITSHM
         ShmRegisterFbFuncs(pScreen);
 #endif
+        pScreenPriv->CloseScreen = pScreen->CloseScreen;
         pScreen->CloseScreen = miCloseScreen;
     }
     /* else CloseScreen */
