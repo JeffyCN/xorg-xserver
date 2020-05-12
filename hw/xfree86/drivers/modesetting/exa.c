440ab7bbfefcb7d959ec88d5000408cd65f12568
Jeffy Chen <jeffy.chen@rock-chips.com>
2020-05-12 09:07:13 +0800

    HACK: modesetting: Use NV12 for dummy output
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- hw/xfree86/drivers/modesetting/exa.c
+++ hw/xfree86/drivers/modesetting/exa.c
@@ -248,7 +248,7 @@ ms_exa_solid(PixmapPtr pPixmap, int x1, int y1, int x2, int y2)
     int height = y2 - y1;
 
     /* skip small images */
-    if (width * height <= 4096)
+    if (!PIXMAP_IS_YUV(pPixmap) && width * height <= 4096)
         goto bail;
 
     if (!rga_prepare_info(pPixmap, &dst_info, x1, y1, x2 - x1, y2 - y1))
@@ -340,7 +340,8 @@ ms_exa_copy(PixmapPtr pDstPixmap, int srcX, int srcY,
     rga_info_t tmp_info = {0};
 
     /* skip small images */
-    if (width * height <= 4096)
+    if (!PIXMAP_IS_YUV(pSrcPixmap) && !PIXMAP_IS_YUV(pDstPixmap) &&
+        width * height <= 4096)
         goto bail;
 
     if (!rga_prepare_info(pSrcPixmap, &src_info, srcX, srcY, width, height))
@@ -537,7 +538,8 @@ ms_exa_composite(PixmapPtr pDst, int srcX, int srcY,
     int sw, sh, blend = 0;
 
     /* skip small images */
-    if (width * height <= 4096)
+    if (!PIXMAP_IS_YUV(pSrc) && !PIXMAP_IS_YUV(pDst) &&
+        width * height <= 4096)
         goto bail;
 
     if (t)
@@ -618,7 +620,7 @@ ms_exa_upload_to_screen(PixmapPtr pDst, int x, int y, int w, int h,
         return FALSE;
 
     /* skip small images */
-    if (w * h <= 4096)
+    if (!PIXMAP_IS_YUV(pDst) && w * h <= 4096)
         return FALSE;
 
     if (!rga_check_pixmap(pDst))
@@ -679,7 +681,7 @@ ms_exa_download_from_screen(PixmapPtr pSrc, int x, int y, int w, int h,
         return FALSE;
 
     /* skip small images */
-    if (w * h <= 4096)
+    if (!PIXMAP_IS_YUV(pSrc) && w * h <= 4096)
         return FALSE;
 
     if (!rga_check_pixmap(pSrc))
