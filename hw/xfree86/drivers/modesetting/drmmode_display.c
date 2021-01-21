377ff97fe550725d99d8574bbe95099bfba263a7
Jeffy Chen <jeffy.chen@rock-chips.com>
2021-01-21 10:01:33 +0800

    modesetting: Support transform for dummy output
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- hw/xfree86/drivers/modesetting/drmmode_display.c
+++ hw/xfree86/drivers/modesetting/drmmode_display.c
@@ -4750,12 +4750,10 @@ drmmode_apply_transform(xf86CrtcPtr crtc)
     else
         drmmode_crtc->is_scale = TRUE;
 
-    if (drmmode_crtc->is_dummy)
-        goto enable_flip_fb;
-
     /* fb flipping disabled or doing shared pixmap flipping */
     if (!drmmode_crtc->can_flip_fb || drmmode_crtc->enable_flipping)
-        goto bail;
+        if (!drmmode_crtc->is_dummy)
+            goto bail;
 
     crtc->driverIsPerformingTransform |= XF86DriverTransformOutput;
 
@@ -4766,10 +4764,10 @@ drmmode_apply_transform(xf86CrtcPtr crtc)
         crtc->driverIsPerformingTransform &= ~XF86DriverTransformOutput;
 
         if (drmmode->fb_flip_mode == DRMMODE_FB_FLIP_TRANSFORMED)
-            goto bail;
+            if (!drmmode_crtc->is_dummy)
+                goto bail;
     }
 
-enable_flip_fb:
     if (!drmmode_enable_flip_fb(crtc)) {
         drmmode_crtc->can_flip_fb = FALSE;
         goto bail;
