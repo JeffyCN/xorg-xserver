fb0e737d4d66760deec4445ded6adb61e0ea6392
Jeffy Chen <jeffy.chen@rock-chips.com>
2021-01-26 15:47:37 +0800

    modesetting: exa: Support multi screens
    By stop using global vars in ms exa.
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- hw/xfree86/drivers/modesetting/drmmode_display.h
+++ hw/xfree86/drivers/modesetting/drmmode_display.h
@@ -89,6 +89,11 @@ enum drmmode_fb_flip_mode {
 };
 
 typedef struct {
+    ExaDriverPtr driver;
+    void *priv;
+} drmmode_exa;
+
+typedef struct {
     int fd;
     unsigned fb_id;
     drmModeFBPtr mode_fb;
@@ -110,7 +115,7 @@ typedef struct {
     OptionInfoPtr Options;
 
     Bool glamor;
-    ExaDriverPtr exa;
+    drmmode_exa *exa;
 
     Bool shadow_enable;
     Bool shadow_enable2;
