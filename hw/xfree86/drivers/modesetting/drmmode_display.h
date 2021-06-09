a25961cf012acbd89150d22b6cd5fb5080b4b8d6
Jeffy Chen <jeffy.chen@rock-chips.com>
2021-06-09 11:02:13 +0800

    modesetting: Add option HotplugReset
    Support resetting re-connected outputs (do a modeset).
    
    This option is enabled by default.
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- hw/xfree86/drivers/modesetting/drmmode_display.h
+++ hw/xfree86/drivers/modesetting/drmmode_display.h
@@ -130,6 +130,8 @@ typedef struct {
     void *shadow_fb;
     void *shadow_fb2;
 
+    Bool hotplug_reset;
+
     enum drmmode_fb_flip_mode fb_flip_mode;
 
     DevPrivateKeyRec pixmapPrivateKeyRec;
@@ -241,6 +243,8 @@ typedef struct {
     uint32_t msc_prev;
     uint64_t msc_high;
     /** @} */
+
+    xf86OutputStatus output_status;
 
     Bool need_modeset;
     struct xorg_list mode_list;
