01c02c7a0d6cd42cdfb7afd03aba7347079f4771
Jeffy Chen <jeffy.chen@rock-chips.com>
2022-09-19 10:26:18 +0800

    modesetting: Support freezing display
    Freeze display when the specified file(from env "XSERVER_FREEZE_DISPLAY")
    exists.
    
    For example:
    export XSERVER_FREEZE_DISPLAY=/tmp/.freeze
    touch $XSERVER_FREEZE_DISPLAY
    X -wr&
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- hw/xfree86/drivers/modesetting/driver.c
+++ hw/xfree86/drivers/modesetting/driver.c
@@ -708,7 +708,7 @@ ms_dirty_get_ent(ScreenPtr screen, PixmapPtr slave_dst)
     return NULL;
 }
 
-static void
+static Bool
 msBlockHandler(ScreenPtr pScreen, void *timeout)
 {
     modesettingPtr ms = modesettingPTR(xf86ScreenToScrn(pScreen));
@@ -716,10 +716,22 @@ msBlockHandler(ScreenPtr pScreen, void *timeout)
     xf86CrtcConfigPtr xf86_config = XF86_CRTC_CONFIG_PTR(pScrn);
     int c;
 
+    /* HACK: Ignore the first request */
+    if (ms->warm_up) {
+        ms->warm_up = FALSE;
+        *((int *)timeout) = 16;
+        return FALSE;
+    }
+
+    if (!access(getenv("XSERVER_FREEZE_DISPLAY") ? : "", F_OK)) {
+        *((int *)timeout) = 16;
+        return FALSE;
+    }
+
     pScreen->BlockHandler = ms->BlockHandler;
     pScreen->BlockHandler(pScreen, timeout);
     ms->BlockHandler = pScreen->BlockHandler;
-    pScreen->BlockHandler = msBlockHandler;
+    pScreen->BlockHandler = (void *)msBlockHandler;
     if (pScreen->isGPU && !ms->drmmode.reverse_prime_offload_mode)
         dispatch_slave_dirty(pScreen);
     else {
@@ -740,6 +752,7 @@ msBlockHandler(ScreenPtr pScreen, void *timeout)
     }
 
     ms_dirty_update(pScreen, timeout);
+    return TRUE;
 }
 
 static void
@@ -748,13 +761,8 @@ msBlockHandler_oneshot(ScreenPtr pScreen, void *pTimeout)
     ScrnInfoPtr pScrn = xf86ScreenToScrn(pScreen);
     modesettingPtr ms = modesettingPTR(pScrn);
 
-    /* HACK: Ignore the first request */
-    if (ms->warm_up) {
-        ms->warm_up = FALSE;
+    if (!msBlockHandler(pScreen, pTimeout))
         return;
-    }
-
-    msBlockHandler(pScreen, pTimeout);
 
     drmmode_set_desired_modes(pScrn, &ms->drmmode, TRUE);
 }
